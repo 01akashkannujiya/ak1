cde5c9cd72be7786cb803d98ec4d2ec8e794d9b0
01akashkannujiya <141305461+01akashkannujiya@users.noreply.github.com>
2023-08-03 12:37:21 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class Addition{
+	public static void main(String[] args)
+	{
+		// initializing variables
+		int num1 = 8, num2 = 4, Add = 0;
+
+		// Displaying num1 and num2
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+		// Remaindering num1 and num2
+		Add = num1 + num2;
+		System.out.println("Addition ="+Add);
+	}
+}
