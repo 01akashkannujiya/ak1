85842520e4224bf87718b2bcb86797d621616356
01akashkannujiya <141305461+01akashkannujiya@users.noreply.github.com>
2023-08-03 12:37:54 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class subtraction{
+	public static void main(String[] args)
+	{
+		// initializing variables
+		int num1 = 8, num2 = 4, Sub = 0;
+
+		// Displaying num1 and num2
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+		// Remaindering num1 and num2
+		Sub = num1 - num2;
+		System.out.println("subtraction ="+Sub);
+	}
+}
