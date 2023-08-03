5a994730038a93ccf835a38187a082361e825bc0
01akashkannujiya <141305461+01akashkannujiya@users.noreply.github.com>
2023-08-03 12:27:27 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class division{
+	public static void main(String[] args)
+	{
+		// initializing variables
+		int num1 = 8, num2 = 4, div = 0;
+
+		// Displaying num1 and num2
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+		// Remaindering num1 and num2
+		div = num1 / num2;
+		System.out.println("division ="+div);
+	}
+}
