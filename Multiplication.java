ee8e78f5c97939e91dc947a458d13ab373d2bfbc
01akashkannujiya <141305461+01akashkannujiya@users.noreply.github.com>
2023-08-03 12:37:39 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class Multiplication{
+	public static void main(String[] args)
+	{
+		// initializing variables
+		int num1 = 8, num2 = 4, Mul = 0;
+
+		// Displaying num1 and num2
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+		// Remaindering num1 and num2
+		Mul = num1 * num2;
+		System.out.println("Multiplication ="+Mul);
+	}
+}
